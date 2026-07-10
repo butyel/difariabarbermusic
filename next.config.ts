85b7bbd5de1ae7462d7555fabebdc8b44317a390
Raphael F. <34407349+butyel@users.noreply.github.com>
2026-07-10 14:05:56 -0300

    Redirect 308 de difariabarbermusic.vercel.app para difariabarbermusic.com.br

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "difariabarbermusic.vercel.app" }],
+        destination: "https://difariabarbermusic.com.br/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
